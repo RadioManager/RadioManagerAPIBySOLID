76cc73db90594bb15811106388372dbd2b8d4064
daniilShestopalov <113010840+daniilShestopalov@users.noreply.github.com>
2026-04-03 14:42:19 +0300

    Init

---
+++ src/main/java/cs/vsu/radiomanagerapibysolid/RadioManagerApiBySolidApplication.java
@@ -0,0 +1,13 @@
+package cs.vsu.radiomanagerapibysolid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioManagerApiBySolidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadioManagerApiBySolidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cs/vsu/radiomanagerapibysolid/RadioManagerApiBySolidApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.vsu.radiomanagerapibysolid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadioManagerApiBySolidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
