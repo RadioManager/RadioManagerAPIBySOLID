b5f9523e1af3f3309ee051eba5370ea0024e8dfe
daniilShestopalov <113010840+daniilShestopalov@users.noreply.github.com>
2026-04-13 16:58:46 +0300

    Created some configs

---
+++ src/main/java/cs/vsu/radiomanagerapibysolid/service/inter/UserService.java
@@ -0,0 +1,35 @@
+package cs.vsu.radiomanagerapibysolid.service.inter;
+
+import cs.vsu.radiomanagerapibysolid.dto.NameDto;
+import cs.vsu.radiomanagerapibysolid.dto.UserDto;
+import cs.vsu.radiomanagerapibysolid.model.enumerate.Role;
+import lombok.NonNull;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<UserDto> getAllUsers();
+
+    UserDto getUserById(Long id);
+
+    List<UserDto> getUsersByRole(Role role);
+
+    UserDto getUserByLogin(String login);
+
+    UserDto createUser(UserDto userDto);
+
+    boolean deleteUser(Long id);
+
+    UserDto updateUser(@NonNull UserDto userDTO);
+
+    boolean updatePassword(Long userId, String newPassword);
+
+    boolean updateBalance(Long userId, Double balance);
+
+    Role getRoleById(Long id);
+
+    NameDto getNameById(Long id);
+
+    UserDto getSystemEntity();
+}
